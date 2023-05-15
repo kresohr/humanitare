7c220301dd4915b1b4f9d7c0fbd2250d8af9e0fa
kresohr <ilicic.kresimir@gmail.com>
2023-05-15 20:29:27 +0200

    Created TutorialActivity page

---
+++ app/src/main/java/com/example/humanitare/view/TutorialActivity.kt
@@ -0,0 +1,12 @@
+package com.example.humanitare.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.humanitare.R
+
+class TutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+    }
+}
